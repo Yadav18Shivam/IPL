7f8f713ce696e4c010194743eec3014b104d9b35
Yadav18Shivam <59693338+Yadav18Shivam@users.noreply.github.com>
2021-08-27 23:03:08 +0530

    Add files via upload

---
+++ implementation/inc/header.h
@@ -0,0 +1,11 @@
+#ifndef __HEADER_H
+#define __HEADER_H
+    void Team_data();
+    void all_player();
+    void schedule_data();
+    void stats_data();
+    void stats_player_data();
+    void umpires_data();
+    void owners_data();
+   
+#endif
